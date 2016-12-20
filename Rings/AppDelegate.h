b5f3db06e2ec5175d8422cfee686028712781e94
FFFFFFMILK <wt1992cuc@gmail.com>
2016-12-20 12:27:49 +0800

    Initial Commit

---
+++ Rings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rings
+//
+//  Created by Fu on 2016/12/20.
+//  Copyright © 2016年 Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
